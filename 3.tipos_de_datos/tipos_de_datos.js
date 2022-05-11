3b90737864bd89e8f38eb16d96e7cfc9c194de15
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-11 14:15:11 +0200

    FEAT 3.tipos_de_datos

---
+++ 3.tipos_de_datos/tipos_de_datos.js
@@ -0,0 +1,22 @@
+// TIPOS DE DATOS
+
+const string = "Esto es un string 💀";
+const numbers = 23;
+const boolean = false;
+// null
+// undefined
+const objeto = {};
+const array = [];
+
+// MEZCLANDO TIPOS DE DATOS;
+
+const mezcla = 10 + 2 + "hola";
+console.log(mezcla);
+
+const mezclaStrings = "hola" + "adios";
+console.log(mezclaStrings);
+
+const activado = true;
+const desactivado = false;
+
+console.log(activado);
