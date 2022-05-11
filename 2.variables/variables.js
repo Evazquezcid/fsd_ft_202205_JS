9368c73d686fc4d31c0b2da1f24ec4f806fa153a
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-11 13:54:08 +0200

    FEAT 2.variables

---
+++ 2.variables/variables.js
@@ -0,0 +1,32 @@
+var x = 10; //Esta es la manera obsoleta
+
+/* LET */
+
+/* let nombre; DECLARAR VARIABLE
+
+nombre = "Paco"; ASIGNAR VALOR */
+let nombre = "Paco"; //Declarando una variable y asignandole un valor
+console.log(nombre);
+nombre = 2;
+console.log(nombre);
+nombre = "Pepito";
+console.log(nombre);
+
+/* CONST */
+const apellido = "Perez";
+console.log(apellido)
+apellido = "Palotes";
+console.log(apellido)
+
+/* TIPS */
+// 1. Los nombres de las variables y constantes tienen que estar en camelCase
+// const Nombre MAL
+// const primer_nombre MAL
+// const NOMBRE MAL
+// const nombre BIEN
+// const primerNombre BIEN
+
+// 2. Utilizad siempre el inglés
+// 3. Que el nombre de la variable sea descriptivo y corto
+
+const name = "Antonio";
